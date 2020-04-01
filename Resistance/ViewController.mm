332c7076b3e0969e6649480a57f7100ccdfc474b
sunshugang <1442140675@qq.com>
2020-04-01 15:21:40 +0800

    已经找到各个颜色所代表的矩形

--- Resistance/ViewController.mm
+++ Resistance/ViewController.mm
@@ -14,6 +14,7 @@
 #import <opencv2/imgproc.hpp>  // Gaussian Blur
 #import <opencv2/videoio.hpp>
 #import <opencv2/highgui.hpp>  // OpenCV window I/O
+#import <Masonry.h>
 
 
 using namespace cv;
@@ -23,6 +24,7 @@ using namespace std;
 @interface ViewController ()<CvVideoCameraDelegate>
 
 @property (nonatomic, strong) CvVideoCamera *videoCamera;
+@property (weak, nonatomic) IBOutlet UILabel *colorName;
 
 @end
 
@@ -39,6 +41,11 @@ using namespace std;
     self.videoCamera.defaultFPS = 30;
     self.videoCamera.grayscaleMode = NO;
     self.videoCamera.delegate = self;
+    [imageView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(self.view);
+        make.centerX.equalTo(self.view);
+        make.size.mas_equalTo(CGSizeMake(352, 288));
+    }];
 
     
     // Do any additional setup after loading the view.
@@ -46,151 +53,122 @@ using namespace std;
 
 - (void)processImage:(cv::Mat &)image
 {
-    //应该是别人用来处理图片，我这里是视频
-//    IplImage* _myimage = cvCreateImage(cvGetSize(image), 8, 3);
-//    cvGetImage(image, _myimage);
-    //输入图片得是三通道彩色图片
-//    assert (!image.empty() && image.channels() == 3);
-
-    if (image.empty()) {
-        return;
-    }
+    //检测矩形部分
+    //转化为灰度图像
+    Mat greyPic;
+    cvtColor(image, greyPic, COLOR_BGR2GRAY); //转化为灰度图
+    medianBlur(greyPic, greyPic, 7);    //中值滤波
+//    image = greyPic;
     
-    if (image.channels() == 4) {
-        Mat hsv;
-        //转化颜色空间的功能
-        cvtColor(image, hsv, COLOR_RGB2HSV);
-
-        //h的范围是0~180，所以选取30个bin
-        //s和v的范围都是0~255，那就选择51个bin
-        //bins：每个特征空间子区段的数目。
-        int hbins = 30;
-        int sbins = 51;
-        int vbins = 51;
-        //histSize, 直方图中每个维度级别数量，比如灰度值（0-255），如果级别数量为4，则灰度值直方图会按照[0, 63],[64,127,[128,191],[192,255]，也称为bin数目，这里是4个bin
-        int hHistSize[] = {hbins};
-        int sHistSize[] = {sbins};
-        int vHistSize[] = {vbins};
-
-        //range：每个特征空间的取值范围。
-        float hranges[] = {0, 180};
-        float sranges[] = {0, 255};
-        float vranges[] = {0, 255};
-        const float* hRanges[] = {hranges};
-        const float* sRanges[] = {sranges};
-        const float* vRanges[] = {vranges};
-        //容器
-        vector<MatND> hist;
-
-        //通道
-        int hChannels[] = {0};
-        int sChannels[] = {1};
-        int vChannels[] = {2};
-        //typedef Mat MatND;
-        MatND hHist, sHist, vHist;
-        //它计算一组数组（通常是图像或图像平面）的直方图
-        //&hsv 图像指针
-        //1 要计算直方图的图像的个数
-        //hChannels 图像的第一个通道，sChannels 图像的第二个通道，vChannels 图像的第三个通道
-        //Mat() 空参数，表示哪些点参与计算
-        //hHist 计算得到的直方图 sHist 计算得到的直方图 vHist 计算得到的直方图
-        //1 得到的直方图的维数，灰度图像为1维，彩色图像为3维
-        //hHistSize hHistSize  hHistSize直方图横坐标的区间数。如果是10，则它会横坐标分为10份，然后统计每个区间的像素点总和。
-        //hRanges sRanges vRanges这是一个二维数组，用来指出每个区间的范围
-        calcHist(&hsv, 1, hChannels, Mat(), hHist, 1, hHistSize, hRanges);
-        calcHist(&hsv, 1, sChannels, Mat(), sHist, 1, sHistSize, sRanges);
-        calcHist(&hsv, 1, vChannels, Mat(), vHist, 1, vHistSize, vRanges);
-        hist.push_back(hHist);
-        hist.push_back(sHist);
-        hist.push_back(vHist);
-        // 直方图归一化
-        //b_hist：输入数组
-        //b_hist：输出归一化数组（可以相同）
-        //0和** histImage.rows：对于这个例子，它们是对r_hist **的值进行归一化的下限和上限
-        //NORM_MINMAX：指示归一化类型的参数（如上所述，它调整之前设置的两个限制之间的值）
-        //** - 1：**意味着输出归一化数组将与输入的类型相同
-        //Mat（）：可选掩码
-        //normalize(b_hist, b_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat() );
-        normalize( hist[0], hist[0], 0, 1, NORM_MINMAX, -1, Mat() );
-        normalize( hist[1], hist[1], 0, 1, NORM_MINMAX, -1, Mat() );
-        normalize( hist[2], hist[2], 0, 1, NORM_MINMAX, -1, Mat() );
-
-        int i;
-        int start = -1, end = -1;
-        for(i = 0; i < 30; i++)
-        {
-            float value = hist[0].at<float>(i);
-            if (value  > 0)
-            {
-                if (start == -1)
-                {
-                    start = i;
-                    end = i;
-                }
-                else
-                    end = i;
-                cout << "H Value" << i << ": " << value << endl;
-            }
-            else
-            {
-                if (start != -1)
-                    cout <<"H:" <<start*6 <<"~"<<(end+1)*6-1<<endl;
-                start = end = -1;
-            }
-        }
-        if (start != -1)
-            cout <<"H:" <<start*5 <<"~"<<(end+1)*5-1<<endl;
-
-        start = -1, end = -1;
-        for(i = 0; i < 51; i++)
-        {
-            float value = hist[1].at<float>(i);
-            if (value  > 0)
-            {
-                if (start == -1)
-                {
-                    start = i;
-                    end = i;
-                }
-                else
-                    end = i;
-                cout << "S Value" << i << ": " << value << endl;
-            }
-            else
-            {
-                if (start != -1)
-                    cout <<"S:"<< start*5 <<"~"<<(end+1)*5-1<<endl;
-                start = end = -1;
-            }
-        }
-        if (start != -1)
-            cout <<"S:" <<start*5 <<"~"<<(end+1)*5-1<<endl;
-
-        start = -1, end = -1;
-        for(i = 0; i < 51; i++)
-        {
-            float value = hist[2].at<float>(i);
-            if (value  > 0)
-            {
-                if (start == -1)
-                {
-                    start = i;
-                    end = i;
-                }
-                else
-                    end = i;
-                cout << "V Value" << i << ": " << value << endl;
-            }
-            else
-            {
-                if (start != -1)
-                    cout <<"V:" <<start*5 <<"~"<<(end+1)*5-1<<endl;
-                start = end = -1;
-            }
-        }
-        if (start != -1)
-            cout <<"V:" <<start*5 <<"~"<<(end+1)*5-1<<endl;
+    Mat binPic;
+//    threshold(greyPic, binPic, 80, 255, THRESH_BINARY);    //阈值化为二值图片
+    const int maxVal = 255;
+    int blockSize = 3;    //取值3、5、7....等
+    int constValue = 10;
+    int adaptiveMethod = 0;
+    int thresholdType = 1;
+    /*
+           自适应阈值算法
+           0:ADAPTIVE_THRESH_MEAN_C
+           1:ADAPTIVE_THRESH_GAUSSIAN_C
+           --------------------------------------
+           阈值类型
+           0:THRESH_BINARY
+           1:THRESH_BINARY_INV
+       */
+       //---------------【4】图像自适应阈值操作-------------------------
+    adaptiveThreshold(greyPic, binPic, maxVal, adaptiveMethod, thresholdType, blockSize, constValue);
+    image = binPic;
+    
+    float cannyThr = 200, FACTOR = 2.5;
+    Mat cannyPic;
+    Canny(binPic, cannyPic, cannyThr, cannyThr*FACTOR);    //Canny边缘检测
+    vector<vector<cv::Point>> contours;    //储存轮廓
+    vector<Vec4i> hierarchy;
+    findContours(cannyPic, contours, hierarchy, RETR_CCOMP, CHAIN_APPROX_SIMPLE);    //获取轮廓
+    
+    //画出图像
+    Mat linePic = Mat::zeros(cannyPic.rows, cannyPic.cols, CV_8UC3);
+    for (int index = 0; index < contours.size(); index++){
+            drawContours(linePic, contours, index, Scalar(rand() & 255, rand() & 255, rand() & 255), 1, 8/*, hierarchy*/);
     }
+//    image = linePic;
+    
+     //暂时不使用//计算最大面积矩形
+//     vector<vector<cv::Point>> polyContours(contours.size());
+//     int maxArea = 0;
+//     for (int index = 0; index < contours.size(); index++){
+//         if (contourArea(contours[index]) > contourArea(contours[maxArea]))
+//             maxArea = index;
+//         approxPolyDP(contours[index], polyContours[index], 10, true);
+//     }
+     //画出矩形
+//    Mat polyPic = Mat::zeros(shrinkedPic.size(), CV_8UC3);
+//    drawContours(polyPic, polyContours, maxArea, Scalar(0,0,255/*rand() & 255, rand() & 255, rand() & 255*/), 2);
+    
+//    Mat matHsv;
+//    cvtColor(image,matHsv,COLOR_BGR2HSV);
+//
+//    vector<int> colorVec;
+//    colorVec.push_back(matHsv.at<uchar>(0,0));
+//    colorVec.push_back(matHsv.at<uchar>(0,1));
+//    colorVec.push_back(matHsv.at<uchar>(0,2));
+//
+//    if((colorVec[0] >= 0 && colorVec[0] <= 180) && (colorVec[1] >= 0 && colorVec[1] <= 255) && (colorVec[2] >= 0 && colorVec[2] <= 46)){
+//        [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+//            self.colorName.text = @"黑";
+//        }];
+//    }
+//    if((colorVec[0] >= 0 && colorVec[0] <= 180) && (colorVec[1] >= 0 && colorVec[1] <= 43) && (colorVec[2] >= 46 && colorVec[2] <= 220)){
+//        [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+//            self.colorName.text = @"灰";
+//        }];
+//    }
+//    if((colorVec[0] >= 0 && colorVec[0] <= 180) && (colorVec[1] >= 0&&colorVec[1] <= 30) && (colorVec[2] >= 221 && colorVec[2] <= 255)){
+//        [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+//            self.colorName.text = @"白";
+//        }];
+//    }
+//    if(((colorVec[0] >= 0 && colorVec[0] <= 10) || (colorVec[0] >= 156 && colorVec[0] <= 180)) && (colorVec[1] >= 43 && colorVec[1] <= 255) && (colorVec[2] >= 46 && colorVec[2] <= 255)){
+//        [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+//            self.colorName.text = @"红";
+//        }];
+//    }
+//    if((colorVec[0] >= 11 && colorVec[0] <= 25) && (colorVec[1] >= 43 && colorVec[1] <= 255) && (colorVec[2] >= 46 && colorVec[2] <= 255)){
+//        [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+//            self.colorName.text = @"橙";
+//        }];
+//    }
+//    if((colorVec[0] >= 26 && colorVec[0] <= 34) && (colorVec[1] >= 43 && colorVec[1] <= 255) && (colorVec[2] >= 46 && colorVec[2] <= 255)){
+//        [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+//            self.colorName.text = @"黄";
+//        }];
+//    }
+//    if((colorVec[0] >= 35 && colorVec[0] <= 77) && (colorVec[1] >= 43 && colorVec[1] <= 255) && (colorVec[2] >= 46 && colorVec[2] <= 255)){
+//        [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+//            self.colorName.text = @"绿";
+//        }];
+//    }
+//    if((colorVec[0] >= 78 && colorVec[0] <= 99) && (colorVec[1] >= 43 && colorVec[1] <= 255) && (colorVec[2] >= 46 && colorVec[2] <= 255)){
+//        [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+//            self.colorName.text = @"青";
+//        }];
+//    }
+//    if((colorVec[0] >= 100 && colorVec[0] <= 124) && (colorVec[1] >= 43 && colorVec[1] <= 255) && (colorVec[2] >= 46 && colorVec[2] <= 255)){
+//        [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+//            self.colorName.text = @"蓝";
+//        }];
+//    }
+//    if((colorVec[0] >= 125 && colorVec[0] <= 155) && (colorVec[1] >= 43 && colorVec[1] <= 255) && (colorVec[2]>=46&&colorVec[2]<=255)){
+//        [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+//            self.colorName.text = @"紫";
+//        }];
+//    }
+//    else{
+//        cout<<"未知"<<endl;
+//    }
+
+   
 }
 - (IBAction)StartAction:(UIButton *)sender {
     [self.videoCamera start];
